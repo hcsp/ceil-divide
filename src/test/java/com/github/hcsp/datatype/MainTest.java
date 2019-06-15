38d7a384e514656b5e99a470ec8f878dee8f2c8a
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/datatype/MainTest.java
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -1,14 +1,13 @@
 package com.github.hcsp.datatype;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
-        assertEquals(2, Main.ceilDivide(6, 3));
-        assertEquals(2, Main.ceilDivide(3, 2));
-        assertEquals(3, Main.ceilDivide(8, 3));
+        Assertions.assertEquals(2, Main.ceilDivide(6, 3));
+        Assertions.assertEquals(2, Main.ceilDivide(3, 2));
+        Assertions.assertEquals(3, Main.ceilDivide(8, 3));
     }
 }
